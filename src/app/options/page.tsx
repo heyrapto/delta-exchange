81ccb2f0933dea33d040272a03dd959ad28b4428
heyrapto <kalejaiyecaleb@gmail.com>
2025-11-04 00:04:26 +0100

    feat: add Banner, Header, and Footer components to OptionsTradePage for improved layout and user experience

--- src/app/options/page.tsx
+++ src/app/options/page.tsx
@@ -1,8 +1,17 @@
+"use client"
+
+import { Footer } from '@/components/layout/footer';
+import { Header } from '@/components/layout/header';
+import { Banner } from '@/components/ui/banner';
 import React from 'react'
 
 const OptionsTradePage = () => {
   return (
     <div>
+       <Banner />
+       <Header />
+       <div></div>
+       <Footer />
     </div>
   )
 }
