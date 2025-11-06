600facb2f3d82f79c63ef6d0534acbefa51da4ba
heyrapto <kalejaiyecaleb@gmail.com>
2025-11-06 05:44:16 +0100

    feat: wrapped app-context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ToastProvider } from "@/providers/toast-provider";
 import { WagmiProviders } from "@/providers/wagmi";
+import { AppContextProvider } from "@/context/app-context";
 
 export const metadata: Metadata = {
   title: "Neostox AI",
@@ -15,9 +16,11 @@ export default function RootLayout({
     <html lang="en">
       <body className="font-aileron antialiased">
         <WagmiProviders>
+          <AppContextProvider>
           <ToastProvider>
             {children}
           </ToastProvider>
+          </AppContextProvider>
         </WagmiProviders>
       </body>
     </html>
