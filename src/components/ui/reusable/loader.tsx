b58672d7a3dd5df16a9635e6167d0da439233b3c
heyrapto <kalejaiyecaleb@gmail.com>
2025-11-06 16:14:28 +0100

    feat: update Loader component with new bouncing animation and primary background color for improved visual appeal

--- src/components/ui/reusable/loader.tsx
+++ src/components/ui/reusable/loader.tsx
@@ -1,8 +1,11 @@
 const Loader = () => {
     return (
       <div className="flex gap-1">
-        <div className="w-3 h-3 bg-gray-500/80 rounded-full animate-[flash_0.5s_ease-out_infinite_alternate]" />
-        <div className="w-3 h-3 bg-gray-500/80 rounded-full animate-[flash_0.5s_ease-out_infinite_alternate] delay-200" />
+        <div className="flex space-x-2">
+                <span className="w-3 h-3 rounded-full animate-bounce [animation-delay:-0.3s]" style={{ backgroundColor: 'var(--button-primary-bg)' }} />
+                <span className="w-3 h-3 rounded-full animate-bounce [animation-delay:-0.15s]" style={{ backgroundColor: 'var(--button-primary-bg)' }} />
+                <span className="w-3 h-3 rounded-full animate-bounce" style={{ backgroundColor: 'var(--button-primary-bg)' }} />
+            </div>
       </div>
     );
   };
